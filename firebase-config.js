3bc262c0a97819e0c86c0a983563f20bf84d1236
nikoniko <68012494+Andrii-zh@users.noreply.github.com>
2025-05-09 21:42:38 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Замініть ці дані на власні з Firebase Console
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCMa2Osai3H26zjoBxRO4Lbqs_4gDkLAfY",
+  authDomain: "city-rating-41816.firebaseapp.com",
+  projectId: "city-rating-41816",
+  storageBucket: "city-rating-41816.firebasestorage.app",
+  messagingSenderId: "691151924109",
+  appId: "1:691151924109:web:7e27c274b112fa8652abc4"
 };
 
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
