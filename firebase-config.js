590b7a746eac61a6510d8d40181d0771c300e518
nikoniko <68012494+Andrii-zh@users.noreply.github.com>
2025-05-09 21:48:01 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,6 @@
+// firebase-config.js
 
-
-// Your web app's Firebase configuration
+// Ваші налаштування Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCMa2Osai3H26zjoBxRO4Lbqs_4gDkLAfY",
   authDomain: "city-rating-41816.firebaseapp.com",
@@ -10,5 +10,5 @@ const firebaseConfig = {
   appId: "1:691151924109:web:7e27c274b112fa8652abc4"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Ініціалізація Firebase для версії 8.x
+firebase.initializeApp(firebaseConfig);
